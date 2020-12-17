b40f8eebeeb4ad91b2fffab33bb61247a6b52c3f
Bruno <bruno03araujo@hotmail.com>
2020-12-17 18:47:49 -0300

    Subtotal e valor total do pedido

--- src/main/java/com/nelioalves/cursomc/services/ProdutoService.java
+++ src/main/java/com/nelioalves/cursomc/services/ProdutoService.java
@@ -36,4 +36,7 @@ public class ProdutoService {
 		List<Categoria> categorias = categoriaRepository.findAllById(ids);
 		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
 	}
+	
+	
+
 }
